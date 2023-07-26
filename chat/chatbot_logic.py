3ab542bbb61d2a2cfaa1ec7312b3f4aad539448e
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-26 19:30:01 +0900

    成果発表会用ロジック

--- chat/chatbot_logic.py
+++ chat/chatbot_logic.py
@@ -1,25 +1,65 @@
 def get_weather(location: str) -> str:
-    weather = {"weather": "晴れ", "temperature": 38.0}
-    print(f"location: {location} / weather: {weather}")
+    weather = {"weather": "晴れ", "temperature": 30.0}
     return f"{location}の天気は、{weather['weather']}。最高気温は{weather['temperature']}度の見込みだよ。"
 
-def add_reminder(remind_at: str, note: str) -> str:
-    reminder = {"remind_at": remind_at, "note": note}
-    print(f"remind_at: {remind_at} / note: {note} / reminder: {reminder}")
-    return f"{reminder['note']}を、{reminder['remind_at']}にお知らせします。"
+def whats_this() -> str:
+    return f"私は、この日のためにpythonで作成されたchatbotアプリです。\n怠惰な発表者に代わりお返事します。"
+
+def made_by_python() -> str:
+    return f"はい、pythonで作成されました。\nインターネット記事と、この後発表する小関さんにもご協力いただき作成できました。"
+
+def hard() -> str:
+    return "一番大変だったのは環境系のエラーが出たときとのことです。"
+
+def how_long() -> str:
+    return "オリジナルアプリの作成に2～3週間。\n私の作成に2日間かかったようです。\n私の開発は手抜きがされているようです。"
+
+def use_ai() -> str:
+    return "いえ、私には使われておりません。\n例えばopenAIのAPIが使用されていれば、より高性能なアプリが作成できます。\n使用にお金がかかるため、私には使っていただけませんでした。"
+
+def lang() -> str:
+    return "Python以外では、Javaも少し勉強しています。\nJavaはカリキュラムが用意されているのでMacの方はやりやすいのではないでしょうか。"
+
+def master() -> str:
+    return "お答えできません。事務所を通してください。"
+
+def stb() -> str:
+    return "抹茶クリームフラペチーノが好物です。\n寒いときは、アールグレイティーラテのヴェンティサイズで、カスタムはオールミルク、ホイップ追加、はちみつ追加が好物です。"
 
 def chat(text: str) -> str:
-    return text
+    return "お答えが用意されていません。発表者の想定を上回る質問でした。\n感服いたしました。"
 
-skills = {"get_weather": get_weather, "add_reminder": add_reminder, "chat": chat}
+skills = {"get_weather": get_weather,
+          "whats_this": whats_this,
+          "made_by_python": made_by_python,
+          "hard": hard,
+          "how_long": how_long,
+          "hard": hard,
+          "use_ai": use_ai,
+          "master": master,
+          "stb": stb,
+          "chat": chat}
 
 # インテント・エンティティの抽出処理
 def extract_intent(text: str) -> tuple:
     if "天気" in text:
-        entities = {"location": "佐賀"}
+        entities = {"location": "東京"}
         return ("get_weather", entities)
-    elif "リマインド" in text:
-        entites = {"remind_at": "2023-07-22T12:34:56", "note": text}
-        return ("add_reminder", entites)
+    elif "なにこれ" in text or "なにそれ" in text or "何これ" in text or "何それ" in text:
+        return ("whats_this", {})
+    elif "python？" in text or "python?" in text:
+        return ("made_by_python", {})
+    elif "大変だった" in text:
+        return ("hard", {})
+    elif "どれくらいかかった" in text:
+        return ("how_long", {})
+    elif "AI" in text or "ai" in text:
+        return ("use_ai", {})
+    elif "言語" in text or "ai" in text:
+        return ("lang", {})
+    elif "発表者" in text:
+        return ("master", {})
+    elif "スタバ" in text:
+        return ("stb", {})
     else:
         return ("chat", {"text": text})
